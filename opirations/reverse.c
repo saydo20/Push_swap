2002850d552971641929e683a16a99ba9d4ab799
saydo20 <jdiasaad86@gmail.com>
2025-12-25 14:25:16 +0100

    small error

--- opirations/reverse.c
+++ opirations/reverse.c
@@ -12,16 +12,56 @@
 
 #include "../push_swap.h"
 
+// void	rra(t_stack *A)
+// {
+// 	t_list	*tmp;
+// 	t_list	*last;
+// 	t_list	*new_last;
+
+// 	tmp = A->head;
+// 	last = A->head;
+// 	if (!A || !A->head)
+// 		return ;
+// 	while (last->next)
+// 		last = last->next;
+// 	new_last = last->prev;
+// 	new_last->next = NULL;
+// 	A->head = last;
+// 	last->next = tmp;
+// 	last->prev = NULL;
+// 	write(1, "rra\n", 4);
+// }
+
+// void	rrb(t_stack *B)
+// {
+// 	t_list	*tmp;
+// 	t_list	*last;
+// 	t_list	*new_last;
+
+// 	tmp = B->head;
+// 	last = B->head;
+// 	if (!B || !B->head)
+// 		return ;
+// 	while (last->next)
+// 		last = last->next;
+// 	new_last = last->prev;
+// 	new_last->next = NULL;
+// 	B->head = last;
+// 	last->next = tmp;
+// 	last->prev = NULL;
+// 	write(1, "rrb\n", 4);
+// }
+
 void	rra(t_stack *A)
 {
 	t_list	*tmp;
 	t_list	*last;
 	t_list	*new_last;
 
+	if (!A || !A->head || !A->head->next)
+		return ;
 	tmp = A->head;
 	last = A->head;
-	if (!A || !A->head)
-		return ;
 	while (last->next)
 		last = last->next;
 	new_last = last->prev;
@@ -29,6 +69,7 @@ void	rra(t_stack *A)
 	A->head = last;
 	last->next = tmp;
 	last->prev = NULL;
+	tmp->prev = last;
 	write(1, "rra\n", 4);
 }
 
@@ -38,10 +79,10 @@ void	rrb(t_stack *B)
 	t_list	*last;
 	t_list	*new_last;
 
+	if (!B || !B->head || !B->head->next)
+		return ;
 	tmp = B->head;
 	last = B->head;
-	if (!B || !B->head)
-		return ;
 	while (last->next)
 		last = last->next;
 	new_last = last->prev;
@@ -49,6 +90,7 @@ void	rrb(t_stack *B)
 	B->head = last;
 	last->next = tmp;
 	last->prev = NULL;
+	tmp->prev = last;
 	write(1, "rrb\n", 4);
 }
 
